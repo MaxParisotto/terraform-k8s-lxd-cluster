dfad5499d965ac513117562c1f3205278ca06668
MaxParisotto <max.parisotto@icloud.com>
2025-07-29 17:45:44 +0000

    Enhance VM provisioning: refactor local variables for better role distribution and add storage disk configurations

--- vms.tf
+++ vms.tf
@@ -12,29 +12,80 @@ data "maas_vm_host" "lxd_hosts" {
   name = each.key
 }
 
-# Provision 2 VMs per node: 1 control-plane, 1 worker
+
+# Provision all required VMs, distributed across 4 nodes
+locals {
+  nodes = ["t42s-node1", "t42s-node2", "t42s-node3", "t42s-node4"]
+  # Distribute roles as evenly as possible
+  control_planes = [for i, node in local.nodes : {
+    name = "cp-${i+1}"
+    hostname = "cp-${i+1}"
+    vm_host = node
+    cores = 4
+    memory = 8192
+    longhorn_disk = false
+  }]
+  workers = [for i, node in local.nodes : {
+    name = "worker-${i+1}"
+    hostname = "worker-${i+1}"
+    vm_host = node
+    cores = 8
+    memory = 16384
+    longhorn_disk = true
+  }]
+  etcds = [for i, node in slice(local.nodes, 0, 3) : {
+    name = "etcd-${i+1}"
+    hostname = "etcd-${i+1}"
+    vm_host = node
+    cores = 2
+    memory = 4096
+    longhorn_disk = false
+  }]
+  mysqls = [for i, node in slice(local.nodes, 0, 3) : {
+    name = "mysql-${i+1}"
+    hostname = "mysql-${i+1}"
+    vm_host = node
+    cores = 2
+    memory = 4096
+    longhorn_disk = false
+  }]
+  vault = [{
+    name = "vault-1"
+    hostname = "vault-1"
+    vm_host = local.nodes[0]
+    cores = 2
+    memory = 4096
+    longhorn_disk = false
+  }]
+  haproxys = [for i, node in slice(local.nodes, 0, 2) : {
+    name = "haproxy-${i+1}"
+    hostname = "haproxy-${i+1}"
+    vm_host = node
+    cores = 2
+    memory = 4096
+    longhorn_disk = false
+  }]
+  all_vms = concat(local.control_planes, local.workers, local.etcds, local.mysqls, local.vault, local.haproxys)
+}
+
 resource "maas_vm_host_machine" "k8s_vms" {
-  for_each = merge(
-    { for node in ["t42s-node1", "t42s-node2", "t42s-node3", "t42s-node4"] :
-        "${node}-cp" => {
-          hostname = "${node}-cp"
-          vm_host  = node
-          cores    = 4
-          memory   = 8192
-        }
-    },
-    { for node in ["t42s-node1", "t42s-node2", "t42s-node3", "t42s-node4"] :
-        "${node}-worker" => {
-          hostname = "${node}-worker"
-          vm_host  = node
-          cores    = 8
-          memory   = 16384
-        }
-    }
-  )
+  for_each = { for vm in local.all_vms : vm.name => vm }
 
   hostname = each.value.hostname
   vm_host  = data.maas_vm_host.lxd_hosts[each.value.vm_host].id
   cores    = each.value.cores
   memory   = each.value.memory
+
+  # Add a 50GB boot disk to all VMs
+  storage_disks {
+    size_gigabytes = 50
+  }
+
+  # Add a 300GB extra disk for Longhorn to workers only
+  dynamic "storage_disks" {
+    for_each = each.value.longhorn_disk == true ? [1] : []
+    content {
+      size_gigabytes = 300
+    }
+  }
 }
